2832d62b04759776ca15c7362b52987df83dd749
Karen. <160618396+Karen2207@users.noreply.github.com>
2025-03-25 12:09:12 -0300

    Add files via upload

--- dados.js
+++ dados.js
@@ -4,22 +4,22 @@ let dados = [
         link : "https://www.garoto.com.br/produtos/caixa-garoto-sortidos-250g"
     },
     {
-        nome : "iPhone",
+        nome : "Lacta",
         descricao : "O iPhone é uma linha de smartphones da Apple, conhecida por sua alta qualidade, sistema operacional iOS exclusivo e recursos avançados de câmera e segurança.",
         link : "https://www.apple.com/iphone"
     },
     {
-        nome : "Mac Air",
+        nome : "Nestlé",
         descricao : "O MacBook Air é um notebook ultraleve da Apple, conhecido por sua portabilidade, design fino e desempenho eficiente, ideal para tarefas do dia a dia e mobilidade.",
-        link : "https://www.apple.com/macbook-air"
+        link : "https://www.americanas.com.br/produto/1673764005/caixa-bombom-especialidades-nestle-251g?pfm_carac=caixa-de-bombom-nestle&pfm_index=1&pfm_page=search&pfm_pos=grid&pfm_type=search_page&offerId=5fc2b4d1762ff367194aa471&cor=Nestle&condition=NEW"
     },
-    {   nome : "Macbook",
+    {   nome : "Ferrero",
         descricao : "O MacBook é uma linha de computadores portáteis desenvolvidos pela Apple, reconhecidos por seu design elegante, desempenho eficiente e recursos inovadores.",
-        link : "https://www.apple.com/macbook"
+        link : "https://www.ferrerorocher.com/br/pt/nossos-produtos/ferrero-rocher/24-unidades"
     },
-    {   nome : "Macbook",
+    {   nome : "Bombons Sortidos",
         descricao : "O MacBook é uma linha de computadores portáteis desenvolvidos pela Apple, reconhecidos por seu design elegante, desempenho eficiente e recursos inovadores.",
-        link : "https://www.apple.com/macbook"
+        link : "https://munik.com.br/produto/bombons-sortidos-300g-24-unidades/"
     },
     {   nome : "Macbook",
         descricao : "O MacBook é uma linha de computadores portáteis desenvolvidos pela Apple, reconhecidos por seu design elegante, desempenho eficiente e recursos inovadores.",
